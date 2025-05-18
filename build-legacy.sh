4dced483cc8c09a051d1cfdd6edcf57c13fe36e1
Robert Wiley <ylee@bodhilinux.com>
2025-05-18 12:44:05 -0400

    Improve legacy build script

--- build-legacy.sh
+++ build-legacy.sh
@@ -3,9 +3,9 @@
 
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd MokshaPink
-sed -i 's/MokshaPink/MokshaPink-Icons/g' default.edc
-sed -i 's/MokshaPink/MokshaPink-GTK/g'   default.edc
-./build.sh
-sed -i 's/MokshaPink-Icons/MokshaPink/g' default.edc
-sed -i 's/MokshaPink-GTK/MokshaPink/g'   default.edc
+sed -i \
+  -e 's/item: "gtk-theme"[ \t]*"[^"]*"/item: "gtk-theme" "MokshaPink-GTK"/' \
+  -e 's/item: "icon-theme"[ \t]*"[^"]*"/item: "icon-theme" "MokshaPink-Icons"/' \
+  default.edc
+git restore default.edc
 cd ..
